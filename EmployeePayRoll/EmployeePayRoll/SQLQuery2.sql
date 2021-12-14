abb5967f13cf29bfb32aaf54cd66010c4cad2ac0
unknown <Saitharala@gmail.com>
2021-12-14 21:48:05 +0530

    [Tharala Sai Prashanth] Added Creating Table Feauture

--- EmployeePayRoll/EmployeePayRoll/SQLQuery2.sql
+++ EmployeePayRoll/EmployeePayRoll/SQLQuery2.sql
@@ -1,3 +1,11 @@
 -- UC-1-create Database
 create database payroll_service1
 use payroll_service1
+
+-- UC-2-create Database
+create table employee_payroll
+( emp_id int not null identity(1,1) primary key,
+  Emp_name varchar(50),
+  salary double precision not null,
+  start date not null,
+  )
